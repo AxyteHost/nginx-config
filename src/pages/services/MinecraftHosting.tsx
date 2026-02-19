5248df4fe9cd209f52ef2598f9eaf612cf0ceadc
Eiro.tf <freakgaming2009@gmail.com>
2026-02-19 13:31:16 +0530

    Update MinecraftHosting.tsx

--- src/pages/services/MinecraftHosting.tsx
+++ src/pages/services/MinecraftHosting.tsx
@@ -8,7 +8,7 @@ const plans: Plan[] = [
     cpu: "100%",
     disk: "5GB NVMe",
     features: ["2 Cloud Backups", "Mod Support", "24/7 Uptime", "DDoS Protection"],
-    buyLink: "https://renderbyte.site/billing/index.php?rp=/store/minecraft/grass-plan",
+    buyLink: "https://renderbyte.site/billing/index.php?rp=/store/minecraft",
   },
   {
     name: "Stone Plan",
@@ -17,7 +17,7 @@ const plans: Plan[] = [
     cpu: "150%",
     disk: "10GB NVMe",
     features: ["3 Cloud Backups", "Mod Support", "24/7 Uptime", "DDoS Protection"],
-    buyLink: "https://renderbyte.site/billing/index.php?rp=/store/minecraft/stone-plan",
+    buyLink: "https://renderbyte.site/billing/index.php?rp=/store/minecraft",
   },
   {
     name: "Coal Plan",
@@ -26,7 +26,7 @@ const plans: Plan[] = [
     cpu: "200%",
     disk: "15GB NVMe",
     features: ["4 Cloud Backups", "Mod Support", "24/7 Uptime", "DDoS Protection"],
-    buyLink: "https://renderbyte.site/billing/index.php?rp=/store/minecraft/coal-plan",
+    buyLink: "https://renderbyte.site/billing/index.php?rp=/store/minecraft",
   },
   {
     name: "Iron Plan",
@@ -36,7 +36,7 @@ const plans: Plan[] = [
     disk: "20GB NVMe",
     popular: true,
     features: ["5 Cloud Backups", "Mod Support", "24/7 Uptime", "DDoS Protection"],
-    buyLink: "https://renderbyte.site/billing/index.php?rp=/store/minecraft/iron-plan-1",
+    buyLink: "https://renderbyte.site/billing/index.php?rp=/store/minecraft",
   },
   {
     name: "Gold Plan",
@@ -45,7 +45,7 @@ const plans: Plan[] = [
     cpu: "300%",
     disk: "25GB NVMe",
     features: ["7 Cloud Backups", "Mod Support", "24/7 Uptime", "DDoS Protection"],
-    buyLink: "https://renderbyte.site/billing/index.php?rp=/store/minecraft/gold-plan",
+    buyLink: "https://renderbyte.site/billing/index.php?rp=/store/minecraft",
   },
   {
     name: "Diamond Plan",
@@ -54,7 +54,7 @@ const plans: Plan[] = [
     cpu: "350%",
     disk: "30GB NVMe",
     features: ["9 Cloud Backups", "Mod Support", "24/7 Uptime", "DDoS Protection"],
-    buyLink: "https://renderbyte.site/billing/index.php?rp=/store/minecraft/diamond-plan",
+    buyLink: "https://renderbyte.site/billing/index.php?rp=/store/minecraft",
   },
   {
     name: "Emerald Plan",
@@ -63,7 +63,7 @@ const plans: Plan[] = [
     cpu: "400%",
     disk: "40GB NVMe",
     features: ["13 Cloud Backups", "Mod Support", "24/7 Uptime", "DDoS Protection"],
-    buyLink: "https://renderbyte.site/billing/index.php?rp=/store/minecraft/emerald-plan",
+    buyLink: "https://renderbyte.site/billing/index.php?rp=/store/minecraft",
   },
   {
     name: "Netherite Plan",
@@ -72,7 +72,7 @@ const plans: Plan[] = [
     cpu: "450%",
     disk: "45GB NVMe",
     features: ["16 Cloud Backups", "Mod Support", "24/7 Uptime", "DDoS Protection"],
-    buyLink: "https://renderbyte.site/billing/index.php?rp=/store/minecraft/netherite-plan",
+    buyLink: "https://renderbyte.site/billing/index.php?rp=/store/minecraft",
   },
   {
     name: "Obsidian Plan",
@@ -81,7 +81,7 @@ const plans: Plan[] = [
     cpu: "450%",
     disk: "45GB NVMe",
     features: ["16 Cloud Backups", "Mod Support", "24/7 Uptime", "DDoS Protection"],
-    buyLink: "https://renderbyte.site/billing/index.php?rp=/store/minecraft/obsidian-plan",
+    buyLink: "https://renderbyte.site/billing/index.php?rp=/store/minecraft",
   },
 ];
 
